bc39414c003a50d16866fc009fc5d0c81e363f35
Dan Fyfe <fyfedaniel@gmail.com>
2019-03-05 19:13:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
   var dwarfCall = '';
-  for (var i = 0; i < (dwarves.length/2); i++){
+  for (var i = dwarves.length/2; i < (dwarves.length/2); i++){
     dwarfCall += `${i + 1}. ${dwarves[i]} `;
   }
   return dwarfCall;
